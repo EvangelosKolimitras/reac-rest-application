eb48273b5a164ab2a85914cd14d0d33b8f0dedf5
Evangelos Kolimitras <evangeloskolimitras@outlook.com>
2020-04-18 00:27:16 +0200

    limit the number of people fetched from a uri

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,24 +1,24 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import App from '../App'
-import Form from './Form'
 const GetData = (props) => {
     const [users, setUsers] = useState([])
     useEffect(() => {
         const fetchData = async () => {
             const users = await axios.get("https://jsonplaceholder.typicode.com/users")
-            setUsers(users.data)
+            const limitedUsers = await users.data.slice(0, 5)
+            setUsers(limitedUsers)
         }
         fetchData()
-    }, [App, Form])
+    }, [App])
     console.log(users)
 
     return (
         <div>
-            <div>Online people</div>
+            <h2>Online fetched people</h2>
             {users.map(user => {
                 return (
-                    <li>{user.name}</li>
+                    <p key={user.id}>{user.name}</p>
                 )
             })}
         </div>
